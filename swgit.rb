6c787e8e43970aed1a8223d36e63ecc806270c4e
9bany <bany.y0599@gmail.com>
2022-05-16 15:09:42 +0700

    v1.1.0-rc5

---
+++ swgit.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class GitSwitch < Formula
+    desc "Easy using git with multiple accounts."
+    homepage ""
+    url "https://github.com/9bany/git-switch/releases/download/v1.1.0-rc5/swgit-macos"
+    sha256 "27573265db43e7f88f47a638976e5112ea266651336fdda8981c3d8a5e48a252"
+    license "MIT"
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test git-switch`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
