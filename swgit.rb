23a7021f266358e6232a2c219ce13188c2b9a903
@bany <53461381+9bany@users.noreply.github.com>
2022-05-19 16:40:49 +0700

    Update swgit.rb

--- swgit.rb
+++ swgit.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Swgit < Formula
-    version "v1.0.0"
+    version "v1.1.0"
     desc "Easy using git with multiple accounts."
     homepage ""
-    url "https://github.com/9bany/git-switch/releases/download/v1.1.0-beta/swgit-macos"
-    sha256 "4b27c922c63fd79e43bc84e43cdb75825278232cbb5523964214c64b0b7f8156"
+    url "https://github.com/9bany/git-switch/releases/download/1.1.0/swgit-macos"      
+    sha256 "2c81237748aa62f7f9bc55c54a7870269afd629f34277d3676bbe1ebbc2c60e7"
     license "MIT"
   
     # depends_on "cmake" => :build
